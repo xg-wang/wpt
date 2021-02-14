4021ce170d042bf8fb2e26db427f48553f87d134
Mike Wasserman <msw@chromium.org>
2021-02-14 14:55:43 -0800

    Window Placement: Implement Screens and ScreenAdvanced interfaces
    Add a RuntimeEnabled Screens interface according to API redesign:
    docs.google.com/document/d/1lgCentReLlym6j9kBS_Qo3r2HHZ6Ov7cSGjSWyi8cYs
    
    Move RuntimeEnabled Window.getScreens() to a WindowScreens supplement.
    Rename the old Window.getScreens() to Window.getScreensDeprecated().
    
    Add a RuntimeEnabled ScreenAdvanced interface inheriting from Screen.
    Make Screen impl non-final with virtual functions for ScreenAdvanced.
    Add ScreenInfo::is_primary & is_internal to expose in ScreenAdvanced.
    
    Add CoreInitializer plumbing to fire Screens.change in modules/.
    
    Pass multi-screen info from browser to renderer via VisualProperties:
    - Replace the singular screen_info with a screen_infos vector.
    - Add display_id to denote the widget's current screen.
    - Add screen_info() for legacy access to the widget's current screen.
    - Validate mojo struct traits in [de]serialization; add tests.
    - Update WidgetBase, ChromeClient, and more code accordingly.
    - Add ScreenInfo::display_id for internal ScreenInfo & Screen tracking.
    
    FIXME: IDL ([measure] decorations; etc.)
    FIXME?: Update Screens::screens_ in OnScreenInfosChanged.
    FOLLOWUP: Update/add more automated tests for the new API.
    FOLLOWUP: Wire up RemoteFrame via FrameVisualProperties, etc.
    FOLLOWUP: Fire Screens.change on connection & currentScreen changes.
    FOLLOWUP: Fire Screen.change on per-Screen[Advanced] attribute changes.
    FOLLOWUP: Add WindowScreens PermissionObserver & set_disconnect_handler?
    FOLLOWUP: Use WebContentsImpl's NativeView in RWHI::GetScreenInfo.
    FOLLOWUP: Remove the old API and plumbing.
    Q: Replace VP::screen_info() w/ screen_infos.begin() or id->info map?
    Q: Plumb display::mojom::Display instead of blink::mojom::ScreenInfo?
    
    Bug: 897300, 1116528
    Test: Automated (more to be updated & added); basic Screens API WAI.
    Change-Id: I1d67cfabda62796274992e3e650d5209dd7bb857

--- screen_enumeration/getScreens.tentative.https.window.js
+++ screen_enumeration/getScreens.tentative.https.window.js
@@ -9,8 +9,11 @@ promise_test(async t => {
 
 promise_test(async t => {
   await test_driver.set_permission({name: 'window-placement'}, 'granted');
-  const screens = await self.getScreens();
+  const screensInterface = await self.getScreens();
+  const screens = screensInterface.screens;
   assert_greater_than(screens.length, 0);
+  assert_equals(screens[0], screensInterface.currentScreen);
+  assert_equals(screens[0], screensInterface.currentScreen);
 
   assert_equals(typeof screens[0].availWidth, 'number');
   assert_equals(typeof screens[0].availHeight, 'number');
@@ -25,16 +28,18 @@ promise_test(async t => {
   assert_equals(typeof screens[0].top, 'number');
   assert_equals(typeof screens[0].orientation, 'object');
 
-  assert_equals(typeof screens[0].primary, 'boolean');
-  assert_equals(typeof screens[0].internal, 'boolean');
-  assert_equals(typeof screens[0].scaleFactor, 'number');
+  assert_equals(typeof screens[0].isExtended, 'boolean');
+  assert_equals(typeof screens[0].isPrimary, 'boolean');
+  assert_equals(typeof screens[0].isInternal, 'boolean');
+  assert_equals(typeof screens[0].devicePixelRatio, 'number');
   assert_equals(typeof screens[0].id, 'string');
-  assert_equals(typeof screens[0].touchSupport, 'boolean');
+  assert_equals(typeof screens[0].pointerTypes, 'object');
+  assert_equals(typeof screens[0].label, 'string');
 }, 'getScreens() returns at least 1 Screen with permission granted');
 
 promise_test(async t => {
   await test_driver.set_permission({name: 'window-placement'}, 'granted');
-  assert_greater_than((await self.getScreens()).length, 0);
+  assert_greater_than((await self.getScreens()).screens.length, 0);
   await test_driver.set_permission({name: 'window-placement'}, 'denied');
   await promise_rejects_dom(t, 'NotAllowedError', self.getScreens());
 }, 'getScreens() rejects the promise with permission denied');
@@ -42,7 +47,7 @@ promise_test(async t => {
 promise_test(async t => {
   await test_driver.set_permission({name: 'window-placement'}, 'granted');
   let iframe = document.body.appendChild(document.createElement('iframe'));
-  assert_greater_than((await iframe.contentWindow.getScreens()).length, 0);
+  assert_greater_than((await iframe.contentWindow.getScreens()).screens.length, 0);
 
   let iframeGetScreens;
   let constructor;
